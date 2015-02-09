5b8bd42d04ba3cf223200687dff9bf44530f7493
Salim Hamed <salimhamed@gmail.com>
2015-02-08 20:10:16 -0800

    Finished final code changes to get all tests to pass.

--- http_server.py
+++ http_server.py
@@ -1,15 +1,32 @@
 import socket
 import sys
+import os
+import mimetypes
+
 
 CRLF = '\r\n'
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+WEBROOT = os.path.join(BASE_DIR, 'webroot')
 
-def response_ok():
-    """returns a basic HTTP response"""
+
+def response_ok(body, mimetype):
+    """returns a HTTP response"""
+    if body is not None and mimetype is not None:
+        resp = []
+        resp.append("HTTP/1.1 200 OK")
+        resp.append("Content-Type: {}".format(mimetype))
+        resp.append("")
+        resp.append(body)
+    else:
+        raise NameError
+    return CRLF.join(resp)
+
+
+def response_not_found():
+    """returns a 405 Not Found response"""
     resp = []
-    resp.append("HTTP/1.1 200 OK")
-    resp.append("Content-Type: text/plain")
+    resp.append("HTTP/1.1 404 Not Found")
     resp.append("")
-    resp.append("this is a pretty minimal response")
     return CRLF.join(resp)
 
 
@@ -30,6 +47,34 @@ def parse_request(request):
     return uri
 
 
+def resolve_uri(uri):
+    """
+    Looks up a resource on disk using the URI and returns that resource.
+    """
+    # map the pathname represented by the URI to a filesystem location
+    resource = os.path.join(WEBROOT, uri.lstrip('/'))
+
+    # if the URI is a directory, it should return a plain-text listing and the
+    # mimetype 'text-plain'
+    if os.path.isdir(resource):
+        content = '\n'.join(os.listdir(resource))
+        type = 'text/plain'
+
+    # if the URI is a file, it should return the contents of that file and its
+    # correct mimetype
+    elif os.path.isfile(resource):
+        with open(resource, 'rb') as f:
+            content = f.read()
+        type = mimetypes.guess_type(resource)[0]
+
+    # if the URI does not map to a real location, it should raise an exception
+    # that the server can catch to return a 404 response
+    else:
+        raise ValueError
+
+    return content, type
+
+
 def server():
     address = ('127.0.0.1', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,7 +86,7 @@ def server():
     try:
         while True:
             print >>sys.stderr, 'waiting for a connection'
-            conn, addr = sock.accept() # blocking
+            conn, addr = sock.accept()  # blocking
             try:
                 print >>sys.stderr, 'connection - %s:%s' % addr
                 request = ""
@@ -56,17 +101,11 @@ def server():
                 except NotImplementedError:
                     response = response_method_not_allowed()
                 else:
-                    # replace this line with the following once you have
-                    # written resolve_uri
-                    response = response_ok()
-                    # content, type = resolve_uri(uri) # change this line
-
-                    ## uncomment this try/except block once you have fixed
-                    ## response_ok and added response_not_found
-                    # try:
-                    #     response = response_ok(content, type)
-                    # except NameError:
-                    #     response = response_not_found()
+                    try:
+                        content, type = resolve_uri(uri)
+                        response = response_ok(content, type)
+                    except (NameError, ValueError):
+                        response = response_not_found()
 
                 print >>sys.stderr, 'sending response'
                 conn.sendall(response)
